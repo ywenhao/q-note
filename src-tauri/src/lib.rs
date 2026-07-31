86b47b66350f1082886d61bda996afe16773f692
ywenhao <actions.win@foxmail.com>
2026-07-31 16:05:17 +0800

    feat: configure signed Tauri updates

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -359,6 +359,8 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(
             tauri_plugin_sql::Builder::default()
                 .add_migrations(&db_url, migrations())
